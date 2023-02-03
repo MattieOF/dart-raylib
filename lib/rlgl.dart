6afc5e0744aa70071e9776a14afd8bd252aeda9b
Matt Ware <mattbware3@gmail.com>
2023-02-03 02:41:14 +0000

    The rlgl functions I need for now

---
+++ lib/rlgl.dart
@@ -0,0 +1,3 @@
+import 'package:raylib/src/generated_bindings.dart';
+
+export 'src/modules/rlgl/rlgl_core.dart';
---
+++ lib/src/modules/rlgl/rlgl_core.dart
@@ -0,0 +1,18 @@
+import 'package:raylib/raylib.dart';
+import 'package:raylib/src/library.dart';
+
+void disableBackfaceCulling() {
+  library.rlDisableBackfaceCulling();
+}
+
+void enableBackfaceCulling() {
+  library.rlEnableBackfaceCulling();
+}
+
+void disableDepthMask() {
+  library.rlDisableDepthMask();
+}
+
+void enableDepthMask() {
+  library.rlEnableDepthMask();
+}
